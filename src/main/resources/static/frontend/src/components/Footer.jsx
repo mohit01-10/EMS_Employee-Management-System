d938fb54d72513eac2f56244ba9a58d798ab5387
Mohit Pal <114811695+mohit01-10@users.noreply.github.com>
2026-01-20 13:41:31 +0530

    Delete src/main/resources/static/frontend directory

--- src/main/resources/static/frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer className="bg-gray-800 text-white text-center p-4 mt-8">
-        <p>&copy; {new Date().getFullYear()} EffiGO EMS. All rights reserved.</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
